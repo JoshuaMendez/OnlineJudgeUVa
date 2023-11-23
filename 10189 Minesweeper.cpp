5fd68bf4bf3e9c5b6d4e9f6d6c9cd8306a81a8cf
Joshua Mendez <joshuamendez0501@gmail.com>
2023-11-23 18:54:12 -0500

    UVa Online Judge

--- 10189 Minesweeper.cpp
+++ 10189 Minesweeper.cpp
@@ -3,12 +3,212 @@
 
 using namespace std;
 
+vector<vector<char>> mines;
+
+char esquinaSupIzq(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        if (mines[i + 1][j + 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char esquinaSupDer(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char esquinaInfIzq(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        if (mines[i - 1][j + 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char esquinaInfDer(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i - 1][j - 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char bordeSup(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        if (mines[i + 1][j + 1] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char bordeIzq(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i - 1][j + 1] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        if (mines[i + 1][j + 1] == '*')
+            intAns++;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char bordeInf(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i - 1][j - 1] == '*')
+            intAns++;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i - 1][j + 1] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char bordeDer(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i - 1][j - 1] == '*')
+            intAns++;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
+char general(int i, int j)
+{
+    char ans;
+    if (mines[i][j] != '*')
+    {
+        int intAns = 0;
+        if (mines[i - 1][j] == '*')
+            intAns++;
+        if (mines[i - 1][j - 1] == '*')
+            intAns++;
+        if (mines[i][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j - 1] == '*')
+            intAns++;
+        if (mines[i + 1][j] == '*')
+            intAns++;
+        if (mines[i + 1][j + 1] == '*')
+            intAns++;
+        if (mines[i][j + 1] == '*')
+            intAns++;
+        if (mines[i - 1][j + 1] == '*')
+            intAns++;
+        ans = '0' + intAns;
+    }
+    else
+        ans = '*';
+    return ans;
+}
+
 int main()
 {
+    int cont = 1;
     int nFilas, nColumnas;
-    vector<vector<char>> mines;
     while (nFilas != 0 && nColumnas != 0)
     {
+        mines.clear();
         cin >> nFilas >> nColumnas;
         for (int i = 0; i < nFilas; i++)
         {
@@ -21,35 +221,65 @@ int main()
             }
             mines.push_back(newLine);
         }
-
-        vector<vector<char>> result;
-
         for (int i = 0; i < nFilas; i++)
         {
             for (int j = 0; j < nColumnas; j++)
             {
-                int add;
+                // Esquinas
                 if (i == 0 && j == 0)
                 {
-                    if (i + 1 == '*')
-                        add++;
-                    else if (j + 1 == '*')
-                        add++;
-                    else if (i + 1 == '*' && j + 1 == '*')
-                        add++;
+                    mines[i][j] = esquinaSupIzq(i, j);
                 }
-                cout << add << endl;
-            }
-        }
+                else if (i == 0 && j == mines[0].size() - 1)
+                {
+                    mines[i][j] = esquinaSupDer(i, j);
+                }
+                else if (i == mines.size() - 1 && j == 0)
+                {
+                    mines[i][j] = esquinaInfIzq(i, j);
+                }
+                else if (i == mines.size() - 1 && j == mines[0].size() - 1)
+                {
+                    mines[i][j] = esquinaInfDer(i, j);
+                }
 
-        cout << "Matriz de Minas:" << endl;
-        for (int i = 0; i < nFilas; i++)
-        {
-            for (int j = 0; j < nColumnas; j++)
-            {
-                cout << mines[i][j];
+                // Bordes
+                else if (i == 0)
+                {
+                    mines[i][j] = bordeSup(i, j);
+                }
+                else if (j == 0)
+                {
+                    mines[i][j] = bordeIzq(i, j);
+                }
+                else if (i == mines.size() - 1)
+                {
+                    mines[i][j] = bordeInf(i, j);
+                }
+                else if (j == mines[0].size() - 1)
+                {
+                    mines[i][j] = bordeDer(i, j);
+                }
+                else
+                {
+                    mines[i][j] = general(i, j);
+                }
             }
-            cout << endl;
         }
+        if (nFilas != 0 && nColumnas != 0)
+        {
+            if (cont != 1)
+                cout << endl;
+            cout << "Field #" << cont << ":" << endl;
+            for (int i = 0; i < nFilas; i++)
+            {
+                for (int j = 0; j < nColumnas; j++)
+                {
+                    cout << mines[i][j];
+                }
+                cout << endl;
+            }
+        }
+        cont++;
     }
 }
